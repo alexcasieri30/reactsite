47f58235250c48a37c8e11091f480748683916e8
alexcasieri <ajc880@nyu.edu>
2022-10-08 18:53:26 -0500

    removed imported images from about

--- src/static_pages/About.jsx
+++ src/static_pages/About.jsx
@@ -1,9 +1,4 @@
-import Me1 from "./images/me_home_1.PNG";
-import Me2 from "./images/me_home_2.PNG";
-import Me3 from "./images/me_home_3.PNG";
-import Me4 from "./images/me_home_4.PNG";
-import Me5 from "./images/me_home_5.PNG";
-import Me6 from "./images/me_home_6.PNG";
+
 import './about.scss';
 import { useEffect } from "react";
 
